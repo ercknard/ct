0fe7dc67552cad2dfdd5167b618a0552aef65021
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-12-03 00:15:20 +0800

    contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -87,45 +87,48 @@ export default function Contact() {
     console.log(fullname, email, subject, message);
   };
   return (
-    <div
-      id="contact"
-      className="py-20 lg:py-[120px] overflow-hidden relative z-10 3xl:w-6/12 lg:w-11/12 w-11/12 justify-center mx-auto"
-    >
-      <div className="container">
-        <div className="flex flex-wrap lg:justify-between -mx-4">
-          <div className="w-full lg:w-1/2 xl:w-6/12 px-4 flex items-center">
-            <div className="max-w-[570px] mb-12 lg:mb-0">
-              <h2
-                className="
+    <>
+      <div className="relative">
+        <div className="contact-svg" />
+        <div
+          id="contact"
+          className="py-20 lg:py-[120px] overflow-hidden relative z-10 3xl:w-6/12 lg:w-11/12 w-11/12 justify-center mx-auto"
+        >
+          <div className="container">
+            <div className="flex flex-wrap lg:justify-between -mx-4">
+              <div className="w-full lg:w-1/2 xl:w-6/12 px-4 flex items-center">
+                <div className="max-w-[570px] mb-12 lg:mb-0">
+                  <h2
+                    className="
                   text-3xl font-bold tracking-tight sm:text-5xl text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-emerald-600"
-              >
-                GET IN TOUCH WITH US
-              </h2>
-              <p className="mt-2 lg:text-2xl text-base text-body-color leading-8 mb-9">
-                LETS TALK ABOUT YOUR PROJECT, Fill the form and send in your
-                queries. We will respond as soon as we can.
-              </p>
-            </div>
-          </div>
-          <div className="w-full lg:w-1/2 xl:w-5/12 px-4">
-            <div className="bg-gray-900 relative rounded-lg pb-8 pt-4 sm:p-12 sm:pt-6 shadow-lg p-4">
-              <h2
-                className="
+                  >
+                    Get in touch with us
+                  </h2>
+                  <p className="mt-2 lg:text-2xl text-base text-body-color leading-8 mb-9">
+                    LETS TALK ABOUT YOUR PROJECT, Fill the form and send in your
+                    queries. We will respond as soon as we can.
+                  </p>
+                </div>
+              </div>
+              <div className="w-full lg:w-1/2 xl:w-5/12 px-4">
+                <div className="bg-gray-900 relative rounded-lg pb-8 pt-4 sm:p-12 sm:pt-6 shadow-lg p-4">
+                  <h2
+                    className="
                   text-2xl text-center mb-4 font-bold tracking-tight sm:text-5xl text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-emerald-600"
-              >
-                Contact Us
-              </h2>
-              <form>
-                <div className="mb-6">
-                  <input
-                    type="text"
-                    placeholder="Your Name"
-                    value={fullname}
-                    onChange={(e) => {
-                      setFullname(e.target.value);
-                    }}
-                    name="fullname"
-                    className="
+                  >
+                    Contact Us
+                  </h2>
+                  <form>
+                    <div className="mb-6">
+                      <input
+                        type="text"
+                        placeholder="Your Name"
+                        value={fullname}
+                        onChange={(e) => {
+                          setFullname(e.target.value);
+                        }}
+                        name="fullname"
+                        className="
                         w-full
                         rounded
                         py-3
@@ -137,21 +140,23 @@ export default function Contact() {
                         focus:border-primary
                         bg-transparent
                         "
-                  />
-                  {errors?.fullname && (
-                    <p className="text-red-500">Fullname cannot be empty.</p>
-                  )}
-                </div>
-                <div className="mb-6">
-                  <input
-                    type="email"
-                    placeholder="Your Email"
-                    name="email"
-                    value={email}
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                    }}
-                    className="
+                      />
+                      {errors?.fullname && (
+                        <p className="text-red-500">
+                          Fullname cannot be empty.
+                        </p>
+                      )}
+                    </div>
+                    <div className="mb-6">
+                      <input
+                        type="email"
+                        placeholder="Your Email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => {
+                          setEmail(e.target.value);
+                        }}
+                        className="
                         w-full
                         rounded
                         py-3
@@ -163,21 +168,21 @@ export default function Contact() {
                         focus:border-primary
                         bg-transparent
                         "
-                  />
-                  {errors?.email && (
-                    <p className="text-red-500">Email cannot be empty.</p>
-                  )}
-                </div>
-                <div className="mb-6">
-                  <input
-                    type="text"
-                    placeholder="Message Subject"
-                    name="subject"
-                    value={subject}
-                    onChange={(e) => {
-                      setSubject(e.target.value);
-                    }}
-                    className="
+                      />
+                      {errors?.email && (
+                        <p className="text-red-500">Email cannot be empty.</p>
+                      )}
+                    </div>
+                    <div className="mb-6">
+                      <input
+                        type="text"
+                        placeholder="Message Subject"
+                        name="subject"
+                        value={subject}
+                        onChange={(e) => {
+                          setSubject(e.target.value);
+                        }}
+                        className="
                         w-full
                         rounded
                         py-3
@@ -189,20 +194,20 @@ export default function Contact() {
                         focus:border-primary
                         bg-transparent
                         "
-                  />
-                  {errors?.subject && (
-                    <p className="text-red-500">Subject cannot be empty.</p>
-                  )}
-                </div>
-                <div className="mb-6">
-                  <textarea
-                    placeholder="Your Message"
-                    name="message"
-                    value={message}
-                    onChange={(e) => {
-                      setMessage(e.target.value);
-                    }}
-                    className="
+                      />
+                      {errors?.subject && (
+                        <p className="text-red-500">Subject cannot be empty.</p>
+                      )}
+                    </div>
+                    <div className="mb-6">
+                      <textarea
+                        placeholder="Your Message"
+                        name="message"
+                        value={message}
+                        onChange={(e) => {
+                          setMessage(e.target.value);
+                        }}
+                        className="
                         w-full
                         rounded
                         py-3
@@ -215,15 +220,17 @@ export default function Contact() {
                         focus:border-primary
                         bg-transparent
                         "
-                  ></textarea>
-                </div>
-                {errors?.message && (
-                  <p className="text-red-500">Message body cannot be empty.</p>
-                )}
-                <div>
-                  <button
-                    type="submit"
-                    className="
+                      ></textarea>
+                    </div>
+                    {errors?.message && (
+                      <p className="text-red-500">
+                        Message body cannot be empty.
+                      </p>
+                    )}
+                    <div>
+                      <button
+                        type="submit"
+                        className="
                         w-full
                         text-white
                         bg-gradient-to-br from-cyan-500 to-emerald-600
@@ -235,30 +242,32 @@ export default function Contact() {
                         text-lg
                         mb-4
                         "
-                  >
-                    Send Message
-                  </button>
+                      >
+                        Send Message
+                      </button>
+                    </div>
+                    <div className="text-left to-space-above">
+                      Alert message :{" "}
+                      <span>
+                        {showSuccessMessage && (
+                          <p className="text-green-500 font-semibold text-sm my-2">
+                            Thankyou! Your Message has been delivered.
+                          </p>
+                        )}
+                        {showFailureMessage && (
+                          <p className="text-red-500">
+                            Oops! Something went wrong, please try again.
+                          </p>
+                        )}
+                      </span>
+                    </div>
+                  </form>
                 </div>
-                <div className="text-left to-space-above">
-                  Alert message :{" "}
-                  <span>
-                    {showSuccessMessage && (
-                      <p className="text-green-500 font-semibold text-sm my-2">
-                        Thankyou! Your Message has been delivered.
-                      </p>
-                    )}
-                    {showFailureMessage && (
-                      <p className="text-red-500">
-                        Oops! Something went wrong, please try again.
-                      </p>
-                    )}
-                  </span>
-                </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
